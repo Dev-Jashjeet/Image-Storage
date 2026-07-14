9f0bddc3620d234cce1451431d40a8f594f656bd
Jashjeet Singh <jashjeet99@gmail.com>
2026-07-14 22:31:49 +0530

    delete image functionality added

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import 'remixicon/fonts/remixicon.css'
 import './App.css'
 import { useEffect, useState } from 'react'
 import { ToastContainer, toast } from 'react-toastify'
-import ImageBox from './ImageBox'
 import type imgObjInterface from './Interface'
 
 function App() {
@@ -34,6 +33,26 @@ function App() {
       return toast.error("Please select image file");
     }
   }
+
+  const imageDownload = (itemObj: imgObjInterface): void => {
+        const a = document.createElement("a");
+        a.href = String(itemObj.imageURL);
+        a.download = itemObj.name;
+        a.click();
+        a.remove();
+        return;
+    }
+
+    const imageDelete = (imageUrl: string) => {
+      const demoImage: imgObjInterface[] = [...images];
+      for(let i=0; i<demoImage.length; i++) {
+        if(demoImage[i].imageURL === imageUrl) {
+          demoImage.splice(i, 1);
+          setImages(demoImage);
+          return toast.success("Image deleted");
+        }
+      }
+    }
   
   return(
     <div className="min-h-screen bg-gray-200">
@@ -47,10 +66,23 @@ function App() {
       </div>
 
       <div className='w-full mt-5 flex justify-center'>
-        <div className='grid grid-cols-4 w-fit p-10 gap-10 sm:grid-cols-1'>
+        <div className='grid xl:grid-cols-4 w-fit p-10 gap-10 max-xl:grid-cols-1'>
           {
             images.map((itemObj: imgObjInterface, unique: number) => (
-              <ImageBox itemObj={itemObj} key={unique}/>
+                <div className='bg-gray-100 border-gray-400 h-fit w-80 relative rounded-2xl hover:scale-102 transition-transform bg-cover' key={unique}>
+                    <img src={String(itemObj.imageURL)} className='w-full rounded-tr-2xl rounded-tl-2xl object-cover' />
+
+                    <div className='flex flex-col p-3 rounded-bl-2xl rounded-br-2xl '>
+                      <div className='flex flex-col'>
+                        <span className='font-bold text-[15px] text-black'>{itemObj.name}</span>
+                        <span className='text-gray-500'>{itemObj.imageSize}Mb</span>
+                      </div>
+                      <span className='flex gap-3 mt-3'>
+                        <span onClick={() => imageDownload(itemObj)} className='text-white cursor-pointer bg-green-500 text-center p-2 w-10 rounded-lg'><i className="ri-download-fill"></i></span>
+                        <span onClick={() => imageDelete(String(itemObj.imageURL))} className='text-white cursor-pointer bg-red-500 text-center p-2 w-10 rounded-lg'><i className="ri-delete-bin-line"></i></span>
+                      </span>
+                    </div>
+                </div>
             ))
           }
         </div>
